8df2f38d0582d18e241c0d8307ef729779a84d0a
Abilio Esteves <eco.abilio@gmail.com>
2023-02-17 12:01:39 -0800

    adds favicon; adds button click to show slash; and fixes slash position wrt dot

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const desiredDistance = 50;
 // Set initial position of the dot and slash in the center of the screen
 let centerX = window.innerWidth / 2;
 let centerY = window.innerHeight / 2;
-let dotOffset = slash.offsetHeight - dot.offsetHeight;
+let dotYOffset = (parseInt(getComputedStyle(slash).height.slice(0,-2), 10)) - (dot.offsetHeight)/2;
+let dotXOffset = (dot.offsetWidth - parseInt(getComputedStyle(slash).width.slice(0,-2), 10))/2;
 let slashTilt = 0.5;
-dot.style.left = centerX + 'px';
-dot.style.top = (centerY + dotOffset) + 'px';
+dot.style.left = (centerX - dotXOffset) + 'px';
+dot.style.top = (centerY + dotYOffset) + 'px';
 
 slash.style.left = centerX + desiredDistance + 'px';
 slash.style.top = centerY + 'px';
 slash.style.transform = `rotate(${slashTilt}rad)`;
+
+dot.onclick = function() {
+  slash.hidden = !slash.hidden
+}
 
 // Track the mouse position and update the slash direction and light beam accordingly
 document.addEventListener('mousemove', (e) => {
@@ -26,8 +31,8 @@ document.addEventListener('mousemove', (e) => {
   centerY = window.innerHeight / 2;
 
   // Update the dot position
-  dot.style.left = centerX + 'px';
-  dot.style.top = (centerY + dotOffset) + 'px';
+  dot.style.left = (centerX - dotXOffset) + 'px';
+  dot.style.top = (centerY + dotYOffset) + 'px';
 
   // Update the slash direction
   const angle = Math.atan2(mouseY - centerY, mouseX - centerX);
